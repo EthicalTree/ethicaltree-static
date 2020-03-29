9153f033d986b5d761885c89b3154a0aa0ae8c2a
William Collins <williamc@surveymonkey.com>
2020-03-29 10:52:39 -0400

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,23 @@
 import React from "react"
 import { Link } from "gatsby"
+import etlogo from '../images/etlogo.svg'
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <img width='300px' src={etlogo} />
+    <h1>It's time to say goodbye</h1>
+    <p>
+      EthicalTree has been a passion project by&nbsp;
+      <a target='_blank' href='https://www.linkedin.com/in/frankjferris/'>Frank Ferris</a>,&nbsp;
+      <a target='_blank' href='https://www.linkedin.com/in/siavash-ghazvinian/'>Siavash Ghazvinian</a>, and&nbsp;
+      <a target='_blank' href='https://www.linkedin.com/in/william-curtis-collins/'>William Collins</a>.
+    </p>
+    <p>
+      Unfortunately, we do not have the means to keep EthicalTree up and running.
+      Thank you to everyone who has supported this project over the years, we couldn't be happier to be involved in this amazing community :)
+    </p>
   </Layout>
 )
 
